722b6cb6f2c7bc10d4d0ef8266c7578c2a6d377b
David Inyangson <much2money@Davids-MacBook-Pro.local>
2018-12-23 23:19:41 -0500

    Fixed home and lucky nav links

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name = "index"),
+    path('', views.index, name = "home_index"),
     path('new', views.post_new, name='post_new')
 ]
