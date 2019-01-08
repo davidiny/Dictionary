a4c75fbcca195b7724cd20225226a15eb1830bde
David Inyangson <much2money@Davids-MacBook-Pro.local>
2019-01-07 20:23:36 -0500

    Finished the website

--- home/views.py
+++ home/views.py
@@ -15,6 +15,10 @@ def index(request):
             search = cd.get('word')
             resp = requests.get('http://api.wordnik.com/v4/word.json/{}/definitions?api_key=14b5420c184640c683005077d1008bba39ae0cd175d218830'.format(search))
             answer = resp.json()
+            if len(answer) == 0: 
+                resp = requests.get('http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=14b5420c184640c683005077d1008bba39ae0cd175d218830')
+                answer = resp.json()
+                return render(request, 'home.html', {'form': form, 'message': True, 'word': answer['word'], 'definition' : answer['definitions'][0]['text'], 'partOfSpeech' : answer['definitions'][0].get('partOfSpeech', "N/A")})
             # html = t.render({'word' : answer[0]['word'], 'definition' : answer[0]['text'], 'partOfSpeech' : answer[0]['partOfSpeech']})
             return render(request, 'definition.html', {'form': form, 'word': answer[0]['word'], 'definition' : answer[0]['text'], 'partOfSpeech' : answer[0].get('partOfSpeech', "N/A")})
     else:
